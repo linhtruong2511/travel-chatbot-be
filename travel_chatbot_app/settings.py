8e05d7f071c0184bc2552acab9434160fa60b921
linh251104 <linht@gmail.com>
2025-10-15 13:51:09 +0700

    update page size

--- travel_chatbot_app/settings.py
+++ travel_chatbot_app/settings.py
@@ -142,7 +142,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 12,
+    'PAGE_SIZE': 9,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
