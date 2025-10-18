f58addad2f2a84cb1cd27dc8c8eae9213d56125a
linh251104 <linht@gmail.com>
2025-10-18 21:58:07 +0700

    update

--- itinerary/urls.py
+++ itinerary/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ItineraryListCreateView, ItineraryDetailView
+from .views import ItineraryListCreateView, ItineraryDetailView, ChangeStatus, GetAllItinerary
 
 urlpatterns = [
     path('', ItineraryListCreateView.as_view(), name='itinerary-list-create'),
     path('<uuid:pk>/', ItineraryDetailView.as_view(), name='itinerary-detail'),
+    path('update-status/<uuid:pk>/', ChangeStatus.as_view(), name='itinerary-update-status'),
+    path('get-all/', GetAllItinerary.as_view(), name='itinerary-get-all'),
 ]
--- itinerary/views.py
+++ itinerary/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
+from rest_framework.views import APIView
 from .models import Itinerary
 from .serializers import ItinerarySerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-
 class ItineraryListCreateView(generics.ListCreateAPIView):
     queryset = Itinerary.objects.all()
     serializer_class = ItinerarySerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def list(self, request, *args, **kwargs):
+        itineraries = Itinerary.objects.filter(author=self.request.user)
+        serializer = ItinerarySerializer(itineraries, many=True)
+        return Response(serializer.data)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -19,3 +24,18 @@ class ItineraryListCreateView(generics.ListCreateAPIView):
 class ItineraryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Itinerary.objects.all()
     serializer_class = ItinerarySerializer
+
+class ChangeStatus(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    def patch(self, request, *args, **kwargs):
+        itinerary = Itinerary.objects.get(pk=kwargs['pk'])
+        itinerary.is_public = request.data.get('is_public')
+        itinerary.save()
+        return Response(status=status.HTTP_200_OK)
+
+class GetAllItinerary(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request, *args, **kwargs):
+        itinerary = Itinerary.objects.filter(is_public=True)
+        serializer = ItinerarySerializer(itinerary, many=True)
+        return Response(serializer.data)
